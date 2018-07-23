4661148363fca7d4f70752aa851f767e0ab1d59b
Joel Guerra <joelaguerra1@gmail.com>
2018-07-23 12:09:30 -0700

    adds comments for scaffold

--- index.py
+++ index.py
@@ -3,11 +3,18 @@ import requests
 
 app = Flask(__name__)
 
+# Fill this tuple with strings of all the Stack Exchange user IDs you'd like to rank on your leaderboard.
+# You can get the User ID from the URL of any profile.
+# For example: https://stackoverflow.com/users/8826629/joel-guerra-msft the ID is 8826629
+
 STACK_IDS=('8826629','9541448', '6591675', '7452904', '9393813', '8840926', '9454112', '9598801', '8797362', '8748848', '9672092', '9603809', '9536851', '8366814', '9541342', '8840650', '9873161', '9401038', '5420074')
 
 # You will need your own Stack Exchange API key
+# Register for one here: https://stackapps.com/apps/oauth/register
+# Replace 'INSERT_YOUR_KEY_HERE' with your key in the string below
+
 def get_user(user_id):
-    res = requests.get("http://api.stackexchange.com/2.2/users/" + user_id + "?order=desc&sort=reputation&site=stackoverflow&key=INSERT_YOUR_KEY_HERE
+    res = requests.get("http://api.stackexchange.com/2.2/users/" + user_id + "?order=desc&sort=reputation&site=stackoverflow&key=INSERT_YOUR_KEY_HERE"
     return res.json()['items'][0]
 
 
